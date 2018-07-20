a0c92f8a7ee9c1de68d0fc9ce9ba813179ec75ac
Aditya Mudgil <adityamudgil2505@gmail.com>
2018-07-21 02:08:56 +0530

    small_alphabets
    Also considering small alphabets in our encrypted message

--- encrypt_automated.py
+++ encrypt_automated.py
@@ -5,18 +5,23 @@
 # need to store encrpted message
 # need to enter key which should be known to both sender and reciever
 
-alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+capital_alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+small_alphabets = capital_alphabets.lower()
 numbers = '1234567890'
 # message = 'I love programming'
 message = input('Enter your message\n')
 encryted_message = ''
 key = 3
-message = message.upper()
 for character in message:
-    if character in alphabets:
-        curr_position = alphabets.find(character)
+    if character in capital_alphabets:
+        curr_position = capital_alphabets.find(character)
         new_position = (curr_position + key) % 26
-        new_character = alphabets[new_position]
+        new_character = capital_alphabets[new_position]
+
+    elif character in small_alphabets:
+        curr_position = small_alphabets.find(character)
+        new_position = (curr_position + key) % 26
+        new_character = small_alphabets[new_position]
 
     elif character in numbers:
         curr_position = numbers.find(character)
